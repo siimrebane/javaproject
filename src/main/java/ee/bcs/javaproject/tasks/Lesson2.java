4d982dc531df1847fd506a91e21903f4831e50fa
Siim Rebane <siim@rebane.org>
2022-11-01 07:20:43 +0200

    Lesson 2

---
+++ src/main/java/ee/bcs/javaproject/tasks/Lesson2.java
@@ -0,0 +1,40 @@
+package ee.bcs.javaproject.tasks;
+
+import java.util.Arrays;
+
+public class Lesson2 {
+    public static void main(String[] args) {
+        System.out.println(Arrays.toString(decreasingArray(9)));
+        // TODO siia saab kirjutada koodi testimiseks
+    }
+
+    // TODO loo massiiv mis saab sisendiks n ja tagastab massiivi suurusega n
+    // Kus esimene element on 1 ja iga järnev arv on 1 võrra suurem
+    // näiteks:
+    // sisend: 5
+    // vastus: {1, 2, 3, 4, 5}
+    public static int[] generateArray(int n) {
+        return new int[0];
+    }
+
+    // TODO
+    // Tagastada massiiv kus oleks numbrid n-ist 0 ini
+    // Näiteks: sisend: 5
+    // Väljund: 5, 4, 3, 2, 1, 0
+    // Näide2: siend: -3
+    // Väljund: -3, -2, -1, 0
+    public static int[] decreasingArray(int n) {
+        return new int[0];
+    }
+
+    // TODO
+    // tagasta massiiv pikkusega n, mille kõigi elementide väärtused on 3
+    public static int[] yl3(int n) {
+        return new int[0];
+    }
+
+    // TODO tagasta massiiv milles oleks numbrid 1,2,3,4,5
+    public static int[] sampleArray() {
+        return new int[0];
+    }
+}
