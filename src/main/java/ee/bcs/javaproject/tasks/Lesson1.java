a2a408a24540d3422bf8ed37d5deb5acb8d0c156
Siim Rebane <siim@rebane.org>
2022-11-01 07:20:21 +0200

    Lesson 1

---
+++ src/main/java/ee/bcs/javaproject/tasks/Lesson1.java
@@ -0,0 +1,49 @@
+package ee.bcs.javaproject.tasks;
+
+
+// TODO kasuta if/else. Ära kasuta Math librarit
+public class Lesson1 {
+    public static void main(String[] args) {
+        System.out.println("Miinimum 2, 4 on: " + min(2, 4));
+        System.out.println("Miinimum 5, 3 on: " + min(5, 3));
+    }
+
+    // TODO tagasta a ja b väikseim väärtus
+    public static int min(int a, int b) {
+        return 0;
+    }
+
+    // TODO tagasta a ja b suurim väärtus
+    public static int max(int a, int b) {
+        return 0;
+    }
+
+    // TODO tagasta a absoluut arv
+    public static int abs(int a) {
+        return 0;
+    }
+
+    // TODO tagasta true, kui a on paaris arv
+    // tagasta false kui a on paaritu arv
+    public static boolean isEven(int a) {
+        return false;
+    }
+
+    // TODO tagasta kolmest arvust kõige väiksem
+    public static int min3(int a, int b, int c) {
+        return 0;
+    }
+
+    // TODO tagasta kolmest arvust kõige suurem
+    public static int max3(int a, int b, int c) {
+        return 0;
+    }
+
+    // TODO
+    //  Tagasta string mille väärtus oleks "\"\\""
+    //  Trüki muutuja sisu välja
+    public static String someString() {
+        return "";
+    }
+
+}
