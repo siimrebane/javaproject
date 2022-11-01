603788ad83499ac9a1558d9af508f893ab22d9b7
Siim Rebane <siim@rebane.org>
2022-11-01 06:54:51 +0200

    Initial commit

---
+++ src/main/java/ee/bcs/javaproject/JavaprojectApplication.java
@@ -0,0 +1,13 @@
+package ee.bcs.javaproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaprojectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ee/bcs/javaproject/JavaprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package ee.bcs.javaproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
