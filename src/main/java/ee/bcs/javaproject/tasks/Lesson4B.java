4bdc7783569e33db13bd8dd8c20ad4da6c6999be
Siim Rebane <siim@rebane.org>
2022-11-16 15:43:02 +0200

    Lesson 4B

---
+++ src/main/java/ee/bcs/javaproject/tasks/Lesson4B.java
@@ -0,0 +1,16 @@
+package ee.bcs.javaproject.tasks;
+
+public class Lesson4B {
+    // DO NOT CHANGE THIS CLASS MODIFY Lesson4
+
+    // Lesson 1
+    // When saving account add firstName and lastName as mandatory fields
+    // When querying accountBalance display firstName, lastName, accountNumber and accountBalance
+
+    // Lesson 2
+    // Add functionality to lock and unlock account
+    // When account is locked depositMoney, withdrawMoney and transferMoney must fail
+
+    // Lesson 3
+    // Add functionalty to query account statement (all deposit, withdraw and transfers) for specific account
+}
