88e0003bb30769811ced34c733b1ff23553cb5b7
Siim Rebane <siim@rebane.org>
2022-11-23 09:51:05 +0200

    Lesson B

---
+++ src/main/java/ee/bcs/javaproject/tasks/LessonB.java
@@ -0,0 +1,69 @@
+package ee.bcs.javaproject.tasks;
+
+public class LessonB {
+
+    public static void main(String[] args) {
+    }
+
+    // TODO trüki numbrid 1 -> 10
+    public static void yl1(){
+
+    }
+
+    // TODO trüki numbrid 10 -> 1
+    public static void yl2(){
+
+    }
+
+    // TODO trüki paaris numberid 2->20
+    public static void yl3(){
+
+    }
+
+    // TODO trüki numbrid 1 -> n
+    public static void yl4(int n){
+
+    }
+
+    // TODO trüki numbrid n -> 1
+    public static void yl5(int n){
+
+    }
+
+    // TODO trüki paaris numberid 2->n
+    public static void yl6(int n){
+
+    }
+
+    public String checkOrder(int a, int b, int c){
+        // Print "increasing" if c > b > a
+        // Print "decreasing" if c < b < a
+        // Print "neither" if none of them is true
+        return "";
+    }
+
+    public boolean checkEqual(int a, int b, int c){
+        // return true if all 3 parameters are the same
+        return false;
+    }
+
+    public boolean isSamll(int a){
+        // return true if the absolute number of a is smaller than 1000
+        return false;
+    }
+
+    // TODO korruta omavahel x ja z ning liida sellele y ja z korrutis
+    public static int testMath(int x, int y, int z){
+        return 0;
+    }
+
+    // TODO kui mitu paaris arvu on sisendites
+    public static int testStuff(int a, int b, int c, int d, int e, int f, int g){
+        return 0;
+    }
+
+    public static boolean userValidation(boolean emailValid, boolean telephoneValid, boolean addressValid){
+        // tagasta true, kui muutuja addressValid on tõene ja vähemalt üks (emailValid või telephoneValid) on tõene
+        return false;
+    }
+}
